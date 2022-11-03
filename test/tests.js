4a8c64826d20055bb6b36fcf71120a18e0be03cb
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-03 11:20:54 -0700

    second function passed three times

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { myFunction } from '../functions.js';
+import { addExclamationPoints, myFunction } from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -22,5 +22,19 @@ skip('this test should be skipped', (expect) => {
 
     const actual = true;
 
+    expect.equal(actual, expected);
+});
+
+test('add 3 exclamation points', (expect) => {
+    const expected = 'hello!!!';
+    const actual = addExclamationPoints('hello');
+    expect.equal(actual, expected);
+
+    const expected2 = 'run!!!';
+    const actual2 = addExclamationPoints('run');
+    expect.equal(actual, expected);
+
+    const expected3 = 'boo!!!';
+    const actual3 = addExclamationPoints('boo');
     expect.equal(actual, expected);
 });
